95dc843e6f5be6f30bb94c5b3d1f245636c5056b
ktlawrence <katuenlaw@gmail.com>
2023-11-10 14:42:09 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,24 +8,41 @@ client = openai.OpenAI()
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 st.title('Chat with AI')
+
+# Use session state to store conversation history
+if 'history' not in st.session_state:
+    st.session_state['history'] = []
+
+# Display previous conversations
+if st.session_state['history']:
+    st.write("Previous Conversations:")
+    for message in st.session_state['history']:
+        st.text_area("", value=message['content'], height=100, key=message['content'])
+
+# User input
 user_input = st.text_input("Talk to the AI")
 
+# Send button
 if st.button('Send'):
+    st.session_state['history'].append({"role": "user", "content": user_input})
+
     try:
         response = client.chat.completions.create(
-            model="gpt-4-1106-preview",
-            messages=[
-                {"role": "system", "content": "You are a helpful assistant."},
-                {"role": "user", "content": f"{user_input}"}
-            ]
+            model="gpt-3.5-turbo",
+            messages=st.session_state['history']
         )
         
-        # Accessing the response content
+        # Extract the AI response and update the history
         if response.choices:
             message_content = response.choices[0].message.content
+            st.session_state['history'].append({"role": "assistant", "content": message_content})
             st.text_area("AI Response:", value=message_content, height=200)
         else:
             st.error("No response received from OpenAI.")
 
     except Exception as e:
         st.error(f"An unexpected error occurred: {e}")
+
+# Reset conversation button
+if st.button('Reset Conversation'):
+    st.session_state['history'] = []
