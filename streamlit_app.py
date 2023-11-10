e6829f75f6f47535143e6a5b6656776835339225
ktlawrence <katuenlaw@gmail.com>
2023-11-10 12:59:24 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,24 @@
 import streamlit as st
 import openai
-# Load your API key from an environment variable or secure source
+# Access the API key from Streamlit secrets
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 st.title('Chat with AI')
-# Create a text box for user input
 user_input = st.text_input("Talk to the AI")
-# When the user submits a question
-
 if st.button('Send'):
-   # Call the OpenAI API with the new chat.completions.create method
-   response = openai.chat.completions.create(
-       model="gpt-3.5-turbo",
-       messages=[
-           {"role": "system", "content": "You are a helpful assistant."},
-           {"role": "user", "content": f"{user_input}"}
-       ]
-   )
-   # Display the model's response
-   st.text_area("AI Response:", value=response['choices'][0]['message']['content'], height=200)
+   try:
+       response = openai.ChatCompletion.create(
+           model="gpt-3.5-turbo",
+           messages=[
+               {"role": "system", "content": "You are a helpful assistant."},
+               {"role": "user", "content": f"{user_input}"}
+           ]
+       )
+       # Check if the response has the expected structure
+       if 'choices' in response and response['choices']:
+           st.text_area("AI Response:", value=response['choices'][0]['message']['content'], height=200)
+       else:
+           st.error("Received an unexpected response format from OpenAI.")
+   except openai.error.OpenAIError as e:
+       st.error(f"An error occurred with the OpenAI API: {e}")
+   except Exception as e:
+       st.error(f"An unexpected error occurred: {e}")
