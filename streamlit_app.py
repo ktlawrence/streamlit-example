a95d56ea4241503955b9d3a0fd03ce94516891f5
ktlawrence <katuenlaw@gmail.com>
2023-11-10 12:05:27 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import openai
 # Load your API key from an environment variable or secure source
-openai.api_key = 'your-api-key'
+openai.api_key = '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'
 st.title('Chat with AI')
 # Create a text box for user input
 user_input = st.text_input("Talk to the AI")
