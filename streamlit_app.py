db4044b9420cde8ab2bfd1dc91c541febfa83145
ktlawrence <katuenlaw@gmail.com>
2023-11-10 12:20:51 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,15 @@ st.title('Chat with AI')
 # Create a text box for user input
 user_input = st.text_input("Talk to the AI")
 # When the user submits a question
+
 if st.button('Send'):
+   # Call the OpenAI API with the new chat.completions.create method
    response = openai.ChatCompletion.create(
-       model="gpt-4",
-       messages=[{"role": "system", "content": "You are a helpful assistant."},
-                 {"role": "user", "content": f"{user_input}"}]
+       model="gpt-3.5-turbo",
+       messages=[
+           {"role": "system", "content": "You are a helpful assistant."},
+           {"role": "user", "content": user_input}
+       ]
    )
    # Display the model's response
    st.text_area("AI Response:", value=response['choices'][0]['message']['content'], height=200)
